d8abdead25cfb2de5f28543892fb071223e2c389
Art San <artsan.mcc@gmail.com>
2025-05-11 22:47:30 +0700

    slip

--- src/features/auth/login.page.tsx
+++ src/features/auth/login.page.tsx
@@ -1,5 +1,25 @@
+import { rqClient } from '@/shared/api/instance'
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/shared/ui/kit/card'
+
 function LoginPage() {
-  return <div>Login page</div>;
+  const loginMutation = rqClient.useMutation('post', '/auth/login')
+  return (
+    <main className=" grow flex flex-col pt-[200px] items-center">
+      <Card className=" w-full max-w-[400px]">
+        <CardHeader>
+          <CardTitle>Вход в систему</CardTitle>
+          <CardDescription>
+            Введите ваш email и пароль для входа в систему
+          </CardDescription>
+        </CardHeader>
+      </Card>
+    </main>
+  )
 }
 
-export const Component = LoginPage;
+export const Component = LoginPage
